6e2317bed00547b7d69ce4f83ab557d6b67a044d
CamiloBallen24 <30605302+CamiloBallen24@users.noreply.github.com>
2018-11-06 13:56:24 -0500

    Hola mundo

---
+++ Scripts/Tuplas.py
@@ -0,0 +1,9 @@
+#Permiten almacener diferentes tipos de datos
+#Las tuplas son inmutables, es decir no se pueden ni agregar ni eliminar elementos
+#Se suelen usar cuando los datos son constantes
+
+myTuple = (1, "String", True)
+print(myTuple)
+print(myTuple[1])
+print()
+print()
