a14bb86e1e0396af51919aa00c954f90ff617400
CamiloBallen24 <30605302+CamiloBallen24@users.noreply.github.com>
2018-11-14 09:20:23 -0500

    Metodos y Atributos

--- Scripts/POO/Clases.py
+++ Scripts/POO/Clases.py
@@ -1,9 +1,26 @@
 #Las clases deben comenzar con mayuscula y  CamellCase
 
+#################################################################
+
 class Lapiz:
+    #Atributos -> Variables
     color = 'Amarillo'
     contieneBorrador = False
     usaGrafito = True
+
+    #Metodos -> Atributos
+    def dibujar(self):
+        print("El lapiz esta dibujando.")
+
+    def borrar(self):
+        if self.esValidoParaBorrar():
+            print("El lapiz esta borrando.")
+        else:
+            print("No es posible borrar.")
+
+    def esValidoParaBorrar(self):
+        return self.contieneBorrador
+
 
 #################################################################
 
@@ -12,3 +29,18 @@ lapizGenrico = Lapiz()
 print(lapizGenrico.color)
 print(lapizGenrico.contieneBorrador)
 print(lapizGenrico.usaGrafito)
+print()
+print()
+print()
+
+
+lapizGenrico.dibujar()
+lapizGenrico.borrar()
+print()
+print()
+print()
+
+lapizGenrico.contieneBorrador = True
+lapizGenrico.borrar()
+
+
