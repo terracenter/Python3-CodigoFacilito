0aa467894fe1e3cdc0df4c31d0024c4d885fd3bc
CamiloBallen24 <30605302+CamiloBallen24@users.noreply.github.com>
2018-11-14 09:10:44 -0500

    Clases y Objetos

---
+++ Scripts/POO/Clases.py
@@ -0,0 +1,14 @@
+#Las clases deben comenzar con mayuscula y  CamellCase
+
+class Lapiz:
+    color = 'Amarillo'
+    contieneBorrador = False
+    usaGrafito = True
+
+#################################################################
+
+#OBJETO = INSTANCIA
+lapizGenrico = Lapiz()
+print(lapizGenrico.color)
+print(lapizGenrico.contieneBorrador)
+print(lapizGenrico.usaGrafito)
