da1276d345d3beeb7827ff10f0b3fed7d62db1bb
CamiloBallen24 <30605302+CamiloBallen24@users.noreply.github.com>
2018-11-15 09:41:32 -0500

    Paquetes

---
+++ Scripts/Paquetes/main.py
@@ -0,0 +1,9 @@
+#TEMA:PAQUETES
+
+########################################################################
+from animals.gato import Gato
+
+
+########################################################################
+gato = Gato("Nuevo gato por paquete")
+print(gato.nombre)
